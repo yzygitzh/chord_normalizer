0d97c617888843405390d961c5d2d083543e8b7b
Ziyue Yang <yzylivezh@hotmail.com>
2016-05-04 22:57:40 +0800

    generating normal music!

--- json_to_midi.py
+++ json_to_midi.py
@@ -7,18 +7,19 @@ output_dir = os.path.abspath("midi_output/")
 midi_velocity = 90
 
 midi_bpm = 120
-tbp = midi_bpm
+tbp = 240
 
-mode_sequence = {"major": [0, 2, 4, 5, 7, 9, 11, 12], \
-                 "minor": [0, 2, 3, 5, 7, 8, 10, 12]}
+mode_sequence = {"major": [0, 0, 2, 4, 5, 7, 9, 11, 12], \
+                 "minor": [0, 0, 2, 3, 5, 7, 8, 10, 12]}
 octave_height = [0, 12, 24, 36, 48, 60, 72, 84]
 
 serialized_dict = {}
 
-def insert_note(note, curr_note_pos):
-    note_length = note["length"] * tbp
+def insert_note(note, curr_note_pos, phrase_pitch):
+    note_length = note["length"]
     tone_pitch_list = map(lambda x: int(x), list(note["sequence"]))
     tone_height_list = map(lambda x: int(x), list(note["height"]))
+    pitch_offset = phrase_pitch - 1
     if note["type"] == "chord":
         for i in range(len(tone_pitch_list)):
             if (1 <= tone_pitch_list[i] and 7 >= tone_pitch_list[i] and \
@@ -27,18 +28,16 @@ def insert_note(note, curr_note_pos):
                 if serialized_dict.has_key(curr_note_pos) == False:
                     serialized_dict[curr_note_pos] = []
                 serialized_dict[curr_note_pos].append(\
-                    [midi_velocity, \
-                     octave_height[tone_height_list[i]] + \
-                     mode_sequence[phrase_class][tone_pitch_list[i]]])
+                    [octave_height[tone_height_list[i]] + pitch_offset + \
+                     mode_sequence[phrase_class][tone_pitch_list[i]], midi_velocity])
                 # insert noteoff
-                if serialized_dict.has_key(curr_note_pos + note_length) == False:
-                    serialized_dict[curr_note_pos] = []
-                serialized_dict[curr_note_pos].append(\
-                    [0, \
-                     octave_height[tone_height_list[i]] + \
-                     mode_sequence[phrase_class][tone_pitch_list[i]]])
+                if serialized_dict.has_key(curr_note_pos + note_length * tbp) == False:
+                    serialized_dict[curr_note_pos + note_length * tbp] = []
+                serialized_dict[curr_note_pos + note_length * tbp].append(\
+                    [octave_height[tone_height_list[i]] + pitch_offset + \
+                     mode_sequence[phrase_class][tone_pitch_list[i]], 0])
     else: # broken chord or melody
-        interval = note_length / len(tone_pitch_list)
+        interval = note_length * tbp / len(tone_pitch_list)
         for i in range(len(tone_pitch_list)):
             if (1 <= tone_pitch_list[i] and 7 >= tone_pitch_list[i] and \
                 1 <= tone_height_list[i] and 7 >= tone_height_list[i]):
@@ -46,16 +45,14 @@ def insert_note(note, curr_note_pos):
                 if serialized_dict.has_key(curr_note_pos) == False:
                     serialized_dict[curr_note_pos] = []
                 serialized_dict[curr_note_pos].append(\
-                    [midi_velocity, \
-                     octave_height[tone_height_list[i]] + \
-                     mode_sequence[phrase_class][tone_pitch_list[i]]])
+                    [octave_height[tone_height_list[i]] + pitch_offset + \
+                     mode_sequence[phrase_class][tone_pitch_list[i]], midi_velocity])
                 # insert noteoff
                 if serialized_dict.has_key(curr_note_pos + interval) == False:
                     serialized_dict[curr_note_pos + interval] = []
                 serialized_dict[curr_note_pos + interval].append(\
-                    [0, \
-                     octave_height[tone_height_list[i]] + \
-                     mode_sequence[phrase_class][tone_pitch_list[i]]])
+                    [octave_height[tone_height_list[i]] + pitch_offset + \
+                     mode_sequence[phrase_class][tone_pitch_list[i]], 0])
             curr_note_pos += interval
 
 root_parent, root_dirnames, root_filenames = os.walk(input_path).next()
@@ -81,54 +78,31 @@ for input_file_name in root_filenames:
         noteset_list = phrase["phrase"]
         for noteset in noteset_list:
             max_note_len = 0
-            for note in noteset:
-                print note
+            for note in noteset:                
                 note_length = note["length"]
                 if note_length > max_note_len:
                     max_note_len = note_length
-                insert_note(note, curr_note_pos)
-            curr_note_pos += max_note_len
+                insert_note(note, curr_note_pos, phrase_pitch)
+            curr_note_pos += max_note_len * tbp
 
-    print serialized_dict
+    note_pos_list = serialized_dict.keys()
+    note_pos_list.sort()
+    
+    for i in range(len(note_pos_list)):
+        note_list = serialized_dict[note_pos_list[i]]
+        if i == 0:
+            for note in note_list:
+                midi_track.append(midi.NoteOnEvent(tick=0, channel=0, data=note))
+        else:
+            # for note in note_list:
+            for j in range(len(note_list)):
+                if j == 0:
+                    midi_track.append(midi.NoteOnEvent(\
+                        tick=note_pos_list[i] - note_pos_list[i - 1], \
+                        channel=0, data=note_list[j]))
+                else:
+                    midi_track.append(midi.NoteOnEvent(tick=0, channel=0, data=note_list[j]))
 
-                    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-pattern = midi.Pattern()
-track = midi.Track()
-pattern.append(track)
-
-on = midi.NoteOnEvent(tick=0, velocity=90, pitch=midi.G_3)
-track.append(on)
-on = midi.NoteOnEvent(tick=0, velocity=90, pitch=midi.E_3)
-track.append(on)
-
-off = midi.NoteOffEvent(tick=10000, pitch=midi.G_3)
-track.append(off)
-off = midi.NoteOffEvent(tick=10000, pitch=midi.E_3)
-track.append(off)
-
-eot = midi.EndOfTrackEvent(tick=1)
-track.append(eot)
-'''
-# Print out the pattern
-# print pattern
-'''
-midi.write_midifile("example.mid", pattern)
-'''
+    midi_track.append(midi.EndOfTrackEvent(tick=1))
+    print midi_pattern
+    midi.write_midifile(output_dir + '/' + output_file_name, midi_pattern)
--- music.f
+++ music.f
@@ -22,4 +22,20 @@ let y = (MakeNoteset
 T = Note@0 -> Note@0;	
 lambda f:T. lambda x:Note@0. f (f x);
 
-ExportPsg p1 => filename;
+ExportPsg p1 => "girigiri_eye";
+
+bc1 = {#"brokenchord"|"13513513"|"44455455"|3#};
+bc2 = {#"brokenchord"|"14614614"|"44455455"|3#};
+bc3 = {#"brokenchord"|"72525525"|"34455455"|3#};
+bc4 = bc1;
+
+ph1 = MakePhrase bc1 bc1;
+ph2 = MakePhrase (MakePhrase ph1 bc2) bc2;
+ph3 = MakePhrase (MakePhrase ph2 bc3) bc3;
+ph4 = MakePhrase (MakePhrase ph3 bc4) bc4;
+
+sg = MakeSegment ph4 (1, "major");
+
+psg = MakePassage sg sg;
+
+ExportPsg psg => "1201";
